7d934a0ca105bc6f40c9ee18c32ba6aa0282dc27
Zeeshan Ahmad <zeeshan.official@gmail.com>
2020-08-11 00:58:49 +0530

    Added solution to LC3_length of longest substring

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,19 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+    let start = 0;
+    let max = 0;
+    let charMap = new Map();
+    for(let i=0; i<s.length; i++) {
+    const currentChar = s[i];
+    if(charMap.get(currentChar) >= start) {
+        start = charMap.get(currentChar) + 1;
+    }
+    charMap.set(currentChar, i);
+    max = Math.max(max, i - start +1);
+    }
+    return max;
+}
 
 module.exports = lengthOfLongestSubstring;
